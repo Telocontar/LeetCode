3568be12ff6c19b9a68d7f21b4ad945279aeae78
telocontar <15180492+Telocontar@users.noreply.github.com>
2022-11-01 23:10:40 +0100

    added helper script to automatically generate test cases from problem webpage

--- helper_script.py
+++ helper_script.py
@@ -1,85 +1,98 @@
-import urllib.request
+from typing import List, Dict, Union
 
-import bs4
-from selenium.webdriver.chrome.service import Service
-
-input_str = """
-Example 1:
-
-Input: nums = [1,2,3,4]
-Output: [1,3,6,10]
-Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
-
-Example 2:
-
-Input: nums = [1,1,1,1,1]
-Output: [1,2,3,4,5]
-Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
-
-Example 3:
-
-Input: nums = [3,1,2,10,1]
-Output: [3,4,6,16,17]
-"""
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
 
-def generate_test_cases(example_str: str, function_name: str):
+def generate_test_cases(examples: List[str], function_name: str) -> str:
 
-    examples = example_str.split("Example ")
+    print("Generating test cases...")
 
-    for example in examples:
-        5
+    # holds the variable initializations for the test cases
+    var_inits = []
+    test_cases = []
 
-    assert 5
+    for i, example in enumerate(examples, start=1):
+        _input = example.split("Input: ")[1].split("\n")[0]
+        var_value = _input.split(" = ")[1]
+        output = example.split("\nOutput: ")[1].split("\n")[0]
+
+        var_init = f"test_input_{i} = {var_value}"
+        var_inits.append(var_init)
+        test_case = f"assert {function_name}(test_input_{i}) == {output}"
+        test_cases.append(test_case)
+
+    test_case_str = "\n".join(var_inits) + "\n\n" + "\n".join(test_cases)
+
+    return test_case_str
+
+
+def get_problem_definitions(page_content: str) -> Dict[str, Union[str, List[str]]]:
+
+    print("Getting problem definitions...")
+
+    problem_text = page_content.split("Add to List\nShare\n")[1].split("\nAccepted\n")[0]
+
+    description = problem_text.split("  Example 1:\n")[0]
+
+    examples = problem_text.split("Example ")[1:]
+    # cut the constraints from the last example
+    examples[-1] = examples[-1].split("\n  Constraints:")[0]
+    constraints = problem_text.split("\n  Constraints:\n")[1]
+
+    result_dict = {
+        "problem_text": problem_text,
+        "description": description,
+        "examples": examples,
+        "constraints": constraints
+    }
+
+    return result_dict
+
+
+def parse_webpage(url: str) -> str:
+
+    print("Preparing webdriver...")
+
+    # do not open browser window
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+
+    # access the url
+    print("Downloading webpage...")
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+                              options=options)
+    driver.get(url)
+
+    # get the body
+    page_content = driver.find_element(By.TAG_NAME, "body").text
+
+    return page_content
+
+
+def print_problem_definition_and_test_cases(url: str, function_name: str):
+
+    page_content = parse_webpage(url=url)
+
+    problem_def_dict = get_problem_definitions(page_content=page_content)
+    problem_text = problem_def_dict["problem_text"]
+    examples = problem_def_dict["examples"]
+
+    test_case_str = generate_test_cases(examples=examples, function_name=function_name)
+
+    print(f'\n"""\n{problem_text}\n"""')
+    print()
+    print(test_case_str)
 
 
 if __name__ == "__main__":
 
+    # contains all problems for category "algorithms"
     leet_code_api_url = "https://leetcode.com/api/problems/algorithms/"
+    leet_code_api_url = "https://leetcode.com/problems/find-pivot-index/"
 
-    # method 1
-    webpage_content = str(urllib.request.urlopen(leet_code_api_url).read())
-
-    # methode 2
-    req = urllib.request.Request(
-        url=leet_code_api_url,
-        headers={"User-Agent": "Mozilla/106.0"}
-    )
-    webpage = urllib.request.urlopen(req).read()
-
-    # method 3
-    class AppURLopener(urllib.request.FancyURLopener):
-        version = "Mozilla/106.0"
-
-    opener = AppURLopener()
-    response = opener.open(leet_code_api_url)
-
-    # method 4
-    from selenium import webdriver
-    from webdriver_manager.chrome import ChromeDriverManager
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.common.by import By
-
-    service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    # driver = webdriver.Firefox()
-    driver.get(leet_code_api_url)
-
-    from selenium import webdriver
-    from selenium.webdriver.chrome.service import Service as ChromeService
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.get(leet_code_api_url)
-
-    from selenium import webdriver
-    from selenium.webdriver.firefox.service import Service as FirefoxService
-    from webdriver_manager.firefox import GeckoDriverManager
-
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    driver.get(leet_code_api_url)
-
-    html = driver.page_source
-    soup = bs4.BeautifulSoup(html, "html.parser")
-
+    print_problem_definition_and_test_cases(url=leet_code_api_url, function_name="pivotIndex")
 
